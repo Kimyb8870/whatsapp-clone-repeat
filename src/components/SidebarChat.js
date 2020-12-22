7a7775f847e2f4238fd8dcfc72bb7e9b102f0211
kimyb8870 <qhd0084@gmail.com>
2020-12-22 14:57:55 +0900

    Feature: set Avatar profile photo in SidebarChat
    use http api from https://avatars.dicebear.com/

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,11 +1,17 @@
 import "./SidebarChat.css";
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SidebarChat = () => {
+  const [seed, setSeed] = useState();
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 100));
+  }, []);
+
   return (
     <div className="sidebarChat">
-      <Avatar />
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
         <h2>Room name</h2>
         <p>last message...</p>
