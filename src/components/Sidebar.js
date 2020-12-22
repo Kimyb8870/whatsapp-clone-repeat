b8f08165c529f185e7cd461062fe6796bf480e7e
kimyb8870 <qhd0084@gmail.com>
2020-12-22 13:52:10 +0900

    Feature: Sidebar__header, Sidebar__search at Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,45 @@
 import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
+import SettingsIcon from "@material-ui/icons/Settings";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { IconButton } from "@material-ui/core";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <SearchOutlinedIcon />
+        <input placeholder="Search or start new chat" />
+      </div>
+      <div className="sidebar__chats">
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+        <h1>Chat Room</h1>
+      </div>
     </div>
   );
 };
