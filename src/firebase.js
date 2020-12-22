44f9e946628394d1d3fbdbd9914d0d055f17b59a
kimyb8870 <qhd0084@gmail.com>
2020-12-22 09:35:28 +0900

    Add: firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCF6FO99aFNEDBiNgV_7UqW0WT8LIpA9go",
+  authDomain: "whatsapp-clone-practice.firebaseapp.com",
+  projectId: "whatsapp-clone-practice",
+  storageBucket: "whatsapp-clone-practice.appspot.com",
+  messagingSenderId: "684588048416",
+  appId: "1:684588048416:web:033e3a4b573c67ad091c45",
+  measurementId: "G-RHF9VNN5ND",
+};
