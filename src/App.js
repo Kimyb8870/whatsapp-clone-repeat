837b6aad7d5d64e37d43d8f969c10d8304042b36
kimyb8870 <qhd0084@gmail.com>
2020-12-22 10:09:07 +0900

    Add: main components Sidebar, Chat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      <h1>Hello React</h1>
       <div className="app__body">
-        {/* Sidebar */}
-        {/* Chat */}
+        <Sidebar />
+        <Chat />
       </div>
     </div>
   );
